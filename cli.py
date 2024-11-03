7dc4d6c424b8c1a1d1c08c5a75790d235a8c7619
devgabrielsborges <devgabrielsborges@proton.me>
2024-11-03 09:27:39 -0300

    Linting

--- cli.py
+++ cli.py
@@ -14,21 +14,33 @@ Usage: python cli.py <search_term> <choice>
 
 '''
 if __name__ == "__main__":
-    search_test = str(argv[1]).strip().lower() if len(argv) > 1 else None
+    print(len(argv))
+    if len(argv) > 3:
+        search_term = str(argv[1]).strip().lower()
+        location = str(argv[2]).strip().lower()
+        choice = argv[3].strip().lower()
+    elif len(argv) > 2:
+        search_term = str(argv[1]).strip().lower()
+        location = None
+        choice = argv[2].strip().lower()
 
-    if search_test is None:
+    if search_term is None:
         print("You must provide a search term")
         exit(1)
-    choice = argv[2].strip().lower() if len(argv) > 2 else None
+
 
     if choice == "jobrapido":
-        jobs = asyncio.run(jobrapido.get_jobrapido(search_test))
-        commons.export_jobs_to_csv("Jobrapido", search_test, jobs)
-        print(f"Exported {len(jobs)} jobs to CSV in jobsData/Jobrapido/{search_test}.csv")
+        jobs = asyncio.run(jobrapido.get_jobrapido(search_term))
+        commons.export_jobs_to_csv("Jobrapido", search_term, jobs)
+        print(f"Exported {len(jobs)} jobs to CSV in jobsData/Jobrapido/{search_term}.csv")
     elif choice == "glassdoor":
-        commons.export_jobs_to_csv("Glassdoor", search_test,
-        glassdoor.get_glassdoor(search_test.replace(" ", "-")))
-        print(f"Exported jobs to CSV in jobsData/Glassdoor/{search_test}")
+        if len(argv) <= 4:
+            search_obj = glassdoor.Glassdoor(search_term, location)
+        search_obj.get_glassdoor_search()
+        search_obj.quit_driver()
+
+        commons.export_jobs_to_csv("Glassdoor", search_term.replace(" ", "_"), search_obj.jobs_list)
+        print(f"Exported jobs to CSV in jobsData/Glassdoor/{search_term}")
     else:
         print("Invalid choice")
         exit(1)
--- glassdoor.py
+++ glassdoor.py
@@ -1,5 +1,6 @@
 import undetected_chromedriver as uc
 from selenium.common.exceptions import TimeoutException, StaleElementReferenceException
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,71 +8,136 @@ from selenium.webdriver.support.ui import WebDriverWait
 from classes import Job
 from commons import set_job_range
 
+class Glassdoor:
+    @staticmethod
+    def _set_driver():
+        driver = uc.Chrome()
 
-def extract_job_details(titles: list, locations: list, companies: list, index: int) -> (str, str, str):
-    try:
-        title = titles[index].text.replace("/", ", ")
-        location = locations[index].text
-        company = companies[index].text
-        return title, location, company
-    except IndexError:
-        return None, None, None
+        return driver
 
-def close_popup(wait):
-    try:
-        close_button = wait.until(ec.element_to_be_clickable((By.CLASS_NAME, 'CloseButton')))
-        close_button.click()
-    except TimeoutException:
-        pass
 
-def get_glassdoor(search: str) -> list:
-    jobs_list = []
-    total_scraped_jobs = 0
+    @staticmethod
+    def _set_wait(driver: object) -> object:
+        wait = WebDriverWait(driver, 15)
 
-    driver = uc.Chrome()
-    driver.get(f"https://www.glassdoor.com.br/Vaga/brasil-{search}-vagas-SRCH_IL.0,6_IN36_KO7,21.htm")
-    wait = WebDriverWait(driver, 15)
+        return wait
 
-    job_quantity_text = wait.until(ec.presence_of_element_located((By.XPATH, '//*[@id="left-column"]/div[1]/h1'))).text
-    job_quantity = set_job_range(job_quantity_text)
 
-    while total_scraped_jobs < job_quantity:
-        titles = driver.find_elements(By.CLASS_NAME, 'JobCard_jobTitle___7I6y')
-        jobs = driver.find_elements(By.CLASS_NAME, 'JobCard_jobCardWrapper__lyvNS')
-        companies = driver.find_elements(By.CLASS_NAME, 'EmployerProfile_compactEmployerName__LE242')
-        locations = driver.find_elements(By.CLASS_NAME, 'JobCard_location__rCz3x')
+    @staticmethod
+    def extract_job_details(titles: list, locations: list, companies: list, index: int) -> (str, str, str):
+        try:
+            title = titles[index].text.replace("/", ", ")
+            location = locations[index].text
+            company = companies[index].text
+            return title, location, company
+        except IndexError:
+            pass
 
-        for i in range(total_scraped_jobs, len(jobs)):
-            jobs[i].click()
 
-            if i == 1:
-                close_popup(wait)
+    def __init__(self, search: str, *location: str | None):
+        self.driver = self._set_driver()
+        self.wait = self._set_wait(self.driver)
+        self.search = search
+        self.location = location
+        self.job_quantity = 0
+        self.jobs_list = []
 
-            title, location, company = extract_job_details(titles, locations, companies, i)
 
-            if title is None:
-                continue
+    def get_home_page(self):
+        # This URL is to avoid login popups. Do not change it.
+        self.driver.get(f"https://www.glassdoor.com.br/Vaga/brasil-java-vagas-SRCH_IL.0,6_IN36_KO7,11.htm")
+
+
+    def home_search(self):
+        search_input = self.wait.until(ec.presence_of_element_located((By.ID, "searchBar-jobTitle")))
+        search_input.send_keys(Keys.CONTROL + "a")
+        search_input.send_keys(Keys.DELETE)
+
+        self.driver.implicitly_wait(5)
+
+        # Search for the job title
+        search_input.send_keys(self.search)
+
+        if self.location:
+            location_input = self.wait.until((ec.presence_of_element_located((By.ID, "searchBar-location"))))
+
+            location_input.send_keys(Keys.CONTROL + "a")
+            location_input.send_keys(Keys.DELETE)
+
+            location_input.send_keys(self.location)
+            location_input.send_keys("\n")
+
+        search_input.send_keys("\n")   # Enter
+
+    def setup_search(self):
+        self.get_home_page()
+        self.home_search()
+        self.get_job_range()
+
+    def get_job_range(self):
+        job_quantity_text = self.wait.until(ec.presence_of_element_located((By.XPATH, '//*[@id="left-column"]/div[1]/h1'))).text
+        job_quantity = set_job_range(job_quantity_text)
+
+        self.job_quantity = job_quantity
+
+    def get_glassdoor_search(self):
+        total_scraped_jobs = 0
+        self.setup_search()
+
+        while total_scraped_jobs < self.job_quantity:
+            titles = self.driver.find_elements(By.CLASS_NAME, 'JobCard_jobTitle___7I6y')
+            jobs = self.driver.find_elements(By.CLASS_NAME, 'JobCard_jobCardWrapper__lyvNS')
+            companies = self.driver.find_elements(By.CLASS_NAME, 'EmployerProfile_compactEmployerName__LE242')
+            locations = self.driver.find_elements(By.CLASS_NAME, 'JobCard_location__rCz3x')
+
+            for i in range(total_scraped_jobs, len(jobs)):
+                self.wait.until(ec.element_to_be_clickable((By.CLASS_NAME, 'JobCard_jobCardWrapper__lyvNS')))
+                jobs[i].click()
+
+                if i == 1:
+                    self.close_popup()
+
+                title, location, company = self.extract_job_details(titles, locations, companies, i)
+
+                if title is None:
+                    continue
+
+                try:
+                    details_button = self.wait.until(ec.element_to_be_clickable((By.CLASS_NAME, 'JobDetails_showMore___Le6L')))
+                    details_button.click()
+                    details = self.wait.until(ec.presence_of_element_located((By.XPATH, '//*[@id="app-navigation"]/div[4]/div[2]/div[2]/div/div[1]')))
+                    _job = Job(title=title, company=company, location=location, description=details.text)
+                    self.jobs_list.append(_job)
+                except TimeoutException as error:
+                    with open("logs.txt", "a") as logs:
+                        logs.write(f"Error finding details: {error}\n")
+
+                total_scraped_jobs += 1
 
             try:
-                details_button = wait.until(ec.element_to_be_clickable((By.CLASS_NAME, 'JobDetails_showMore___Le6L')))
-                details_button.click()
-                details = wait.until(ec.presence_of_element_located((By.XPATH, '//*[@id="app-navigation"]/div[4]/div[2]/div[2]/div/div[1]')))
-                _job = Job(title=title, company=company, location=location, description=details.text)
-                jobs_list.append(_job)
-            except TimeoutException as error:
-                with open("logs.txt", "a") as logs:
-                    logs.write(f"Error finding details: {error}\n")
+                more_jobs = self.wait.until(ec.element_to_be_clickable((By.XPATH, '//*[@id="left-column"]/div[2]/div/button')))
+                more_jobs.click()
+            except StaleElementReferenceException:
+                more_jobs = self.wait.until(ec.element_to_be_clickable((By.XPATH, '//*[@id="left-column"]/div[2]/div/button')))
+                more_jobs.click()
+            except TimeoutException:
+                break
 
-            total_scraped_jobs += 1
 
+    def close_popup(self):
         try:
-            more_jobs = wait.until(ec.element_to_be_clickable((By.XPATH, '//*[@id="left-column"]/div[2]/div/button')))
-            more_jobs.click()
-        except StaleElementReferenceException:
-            more_jobs = wait.until(ec.element_to_be_clickable((By.XPATH, '//*[@id="left-column"]/div[2]/div/button')))
-            more_jobs.click()
+            close_button = self.wait.until(ec.element_to_be_clickable((By.CLASS_NAME, 'CloseButton')))
+            close_button.click()
         except TimeoutException:
-            break
+            pass
 
-    driver.quit()
-    return jobs_list
+    def quit_driver(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    search_term = "estágio python"
+    location = "recife"
+    search_obj = Glassdoor(search_term)
+    search_obj.get_glassdoor_search()
+    search_obj.quit_driver()
+    print(f"Exported jobs to CSV in jobsData/Glassdoor/{search_term}")
